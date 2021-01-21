489579666fd264c0248e1ef77bfb21492743947e
codingcat <istommao@gmail.com>
2021-01-21 11:28:03 +0800

    Modify setup.py long_description

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
 # -*- coding: utf-8 -*-
 """setup.py."""
+import os
+
 from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Get the long description from the README file
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as fd:
+    long_description = fd.read()
+
 
 INSTALL_REQUIRES = [
 ]
-VERSION = '0.0.1'
+VERSION = '0.0.3'
 
 setup(
     name='onelibs',
     version=VERSION,
     description='onelibs is an out-of-the-box libs written in python',
+    long_description=long_description,
     author='codingcat',
     author_email='istommao@gmail.com',
     install_requires=INSTALL_REQUIRES,
